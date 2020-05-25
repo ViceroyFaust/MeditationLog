ad531b6ad5f6bf181ded05e27d196ea109868a3a
dfowl <d.ribchinski@gmail.com>
2020-05-25 09:20:07 -0400

    Implemented a dateSearch function

--- MeditationLog/src/Logger.java
+++ MeditationLog/src/Logger.java
@@ -7,14 +7,15 @@ import java.io.PrintWriter;
 
 public class Logger {
 
+	private static final File file = new File("data.csv");
+	
 	public static void addLog(String date, String length) throws IOException {
-		PrintWriter write = new PrintWriter(new FileWriter(new File("data.csv"), true));
+		PrintWriter write = new PrintWriter(new FileWriter(file, true));
 		write.printf("%s,%s%n", date, length);
 		write.close();
 	}
 	
 	public static void removeLog(String date, String length) throws IOException {
-			File file = new File("data.csv");
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			StringBuilder builder = new StringBuilder();
 			String line;
@@ -24,12 +25,27 @@ public class Logger {
 					delete--;
 					continue;
 				}
-				builder.append(line + "\n");
+				builder.append(line);
+				builder.append("\n");
 			}
 			System.out.println(builder.toString());
 			reader.close();
 			FileWriter write = new FileWriter(file);
 			write.write(builder.toString());
 			write.close();
+	}
+	
+	public static String dateSearch(String date) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		StringBuilder builder = new StringBuilder();
+		String line;
+		while((line = reader.readLine()) != null) {
+			if (line.contains(date)) {
+				builder.append(line);
+				builder.append("\n");
+			}
+		}
+		reader.close();
+		return builder.toString();
 	}
 }
